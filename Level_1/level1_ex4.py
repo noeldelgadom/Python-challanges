e7b2fe0d16f8290d9f2af359ea8d7af2f6910c10
Noel Delgado <noeldelgado89@hotmail.com>
2017-04-08 02:47:54 +0000

    resolví level1_ex4

--- Level_1/level1_ex4.py
+++ Level_1/level1_ex4.py
@@ -8,3 +8,35 @@ and ask if the players want to start a new game
 
 
 """
+
+rematch = 'y'
+tie = 'Its a tie'
+aWins = 'Player A wins!'
+bWins = 'Player B wins!'
+
+while rematch is 'y':
+    a = input('Player A - chose r,p,s: ')
+    b = input('Player B - chose r,p,s: ')
+    if a is 'r':
+        if b is 'r':
+            print(tie)
+        elif b is 'p':
+            print(bWins)
+        elif b is 's':
+            print(aWins)
+    elif a is 'p':
+        if b is 'r':
+            print(aWins)
+        elif b is 'p':
+            print(tie)
+        elif b is 's':
+            print(bWins)
+    elif a is 's':
+        if b is 'r':
+            print(bWins)
+        elif b is 'p':
+            print(aWins)
+        elif b is 's':
+            print(tie)
+    
+    rematch = input('Want to play again?(y/n):')
