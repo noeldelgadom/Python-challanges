a263f7062109d7ecd0483856adf3f6c0299fac1d
Noel Delgado <noeldelgado89@hotmail.com>
2017-04-10 15:46:23 +0000

    Improved level2_ex3

--- Level_2/level2_ex3.py
+++ Level_2/level2_ex3.py
@@ -1,7 +1,7 @@
 """
-Write a a generator in Python. Be creative with how you generate 
-as - strong as have a mix of lowercase letters, uppercase letters, 
-numbers, and symbols. The as should be random, generating a new a 
+Write a password generator in Python. Be creative with how you generate 
+passwords - strong passwords have a mix of lowercase letters, uppercase letters, 
+numbers, and symbols. The passwords should be random, generating a new password 
 every time the user asks for a new a. 
 Include your run-time code in a main method.
 
@@ -19,44 +19,43 @@ import string
 
 def main():
     newPassword = True
+    
     while newPassword:
         
         # Prompt user for password strength
-        pStrength = input("How strong do you want your password to be? (weak, medium, strong): ")
+        pStrength = input("What password strength do you want? (weak, strong): ")
+        
         if pStrength == 'weak':
-            passwordLength = 6
-        elif pStrength == 'medium':
-            passwordLength = 8
-        elif pStrength == 'strong':
-            passwordLength = 10
+            worldList = ['bird', 'home', 'rock', 'fork', 'walk', 'eat', 'jump']
+            password = random.choice(worldList)
+            passwordLength = len(password)
+        else:
+            a = ''      # a will be a string with at least one lowercase, uppercase, number and symbol
+            a += random.choice(string.ascii_letters).lower()
+            a += random.choice(string.ascii_letters).upper()
+            a += str(random.randrange(10))
+            a += random.choice(string.punctuation)
             
-        a = ''      # a will be a string that is randomly generated. 
-        for i in range(passwordLength - 4):
-            character = random.randrange(4)
-            if character == 0:
-                a += random.choice(string.ascii_letters).lower()
-            elif character == 1:
-                a += random.choice(string.ascii_letters).upper()
-            elif character == 2:
-                a += str(random.randrange(10))
-            elif character == 3:
-                a += random.choice(string.punctuation)
-    
-        b = ''      # b will be a string with at least one lowercase, uppercase, number and symbol
-        b += random.choice(string.ascii_letters).lower()
-        b += random.choice(string.ascii_letters).upper()
-        b += str(random.randrange(10))
-        b += random.choice(string.punctuation)
-        
-        # Join a + b and shuffle the sum
-        password = list(a + b)
-        random.shuffle(password)
-        password = ''.join(password)
-        print('Your randomly generated %s strength password has %s characters:   %s' % (pStrength, passwordLength, password))
-        
+            b = ''      # b will be a randomly generated string with 4 characters
+            for i in range(4):
+                character = random.randrange(4)
+                if character is 0:
+                    b += random.choice(string.ascii_letters).lower()
+                elif character is 1:
+                    b += random.choice(string.ascii_letters).upper()
+                elif character is 2:
+                    b += str(random.randrange(10))
+                elif character is 3:
+                    b += random.choice(string.punctuation)
+                    
+            # Join a + b and shuffle the sum
+            password = list(a + b)
+            random.shuffle(password)
+            password = ''.join(password)
+            
+        print('Your randomly generated %s password has %s characters:   %s' % (pStrength, 8, password))
         newPassword = input("Do you want to generate a new one? (y/n): ")
         newPassword = True if newPassword is 'y' else False
-        
     
 if __name__ == '__main__':
     main()
